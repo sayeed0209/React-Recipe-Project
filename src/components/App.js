95e281387aced29e12965c21f97573cb47f164be
abu sayeed <sayeed0209@gmail.com>
2021-06-14 19:03:48 +0200

    added default searchterm

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,24 @@ import SearchBar from "./SearchBar";
 import MealList from "./MealList";
 class App extends React.Component {
 	state = { meals: [], seletcedImage: null };
+	
 	onFromSubmitApp = async searchTerm => {
 		const res = await axios.get(
 			`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
 		);
-		this.setState({ meals: res.data.meals, seletcedImage: res.data.meals[0] });
+		if(res.data.meals){
+			this.setState({ meals: res.data.meals, seletcedImage: res.data.meals[0] });
+		}else{
+			alert('there is no meal found with this search term')
+		}
+		
 	};
 	onImageClick = image => {
 		this.setState({ seletcedImage: image });
 	};
-
+	componentDidMount(){
+		this.onFromSubmitApp('chicken')
+	}
 	render() {
 		return (
 			<div className='ui container'>
